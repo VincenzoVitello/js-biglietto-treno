ae2c3ae89e6c944c3a72c5299060c4b81f3254fd
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2021-12-03 14:52:20 +0100

    progetto finito

---
+++ myscript.js
@@ -0,0 +1,16 @@
+let miles = prompt ('quanti km percorrerai?');
+let age = prompt ('quanti anni hai?');
+
+if(age < 18){
+    scount = 20;
+}else if(age >= 65){
+    scount = 45;
+}else{
+    scount = 1;
+}
+
+let price = 0.21 * miles;
+let discount = (price / 100) * scount;
+let discountedPrice = (price - discount);
+
+document.getElementById('ticket_price').innerHTML = Math.ceil(discountedPrice)+ '€';
